c9a3e83f7afb3bdfc1168a1394c0ed9c4ad2ed1d
Fynmar91 <8vIX2sfR>
2019-12-05 11:53:51 +0100

    Projektdateien hinzufügen.

---
+++ Spiel/Spiellogik.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Spiel
+{
+	class Spiellogik
+	{
+		static public Point Rasteruebersetzung(Canvas spielbrett, Point punkt)
+		{
+			double breite = spielbrett.ActualWidth / 16;
+			double hoehe = spielbrett.ActualHeight / 16;
+			Point koordinate = new Point((int)(punkt.X / 16), (int)(punkt.Y / 16));
+
+			return koordinate;
+		}
+	}
+}
